812795a7698b09639367e858108e877a74e0b381
GnouhPTV <nhocphuongtk3@gmail.com>
2023-11-08 16:14:38 +0700

    Valorant stats

---
+++ README.md
@@ -0,0 +1,43 @@
+<h1 align="center">Valorant Stats Discord Bot</h1>
+<p align="center">
+  <b>Verified by Discord</b>
+      <img src="https://emoji.gg/assets/emoji/6817_Discord_Verified.png" width="12" height="12"><br>
+  <a href="https://discord.com/api/oauth2/authorize?client_id=833535533287866398&permissions=2417093728&scope=bot">Invite this bot to your server</a> 
+   <br>Used in over 1,600 Discord servers<br>
+</p>
+
+---
+# Screenshots 📷
+![stats](https://user-images.githubusercontent.com/76707560/123704292-83bc0080-d833-11eb-8256-fbc30e317e18.png)
+![lastmatch](https://user-images.githubusercontent.com/76707560/123704304-8585c400-d833-11eb-828c-06a246597104.png)
+![agentandmap](https://user-images.githubusercontent.com/76707560/123704310-86b6f100-d833-11eb-92cd-102cc42c63d9.png)
+
+
+# Commands ⌨️
+| Command        | Description    | 
+| :-----------: | :--------: | 
+| v!help      | Display a list of avaliable commands      | 
+| v!competitive    | Display competitive career stats of a Valorant player | 
+| v!unrated     | Display unrated career stats of a Valorant player | 
+| v!spikerush    | Display spike rush career stats of a Valorant player | 
+| v!deathmatch    | Display deathmatch career stats of a Valorant player | 
+| v!escalation    | Display escalation career stats of a Valorant player | 
+| v!lm      | Display last match information of a Valorant player |  
+| v!agents  | Display agent stats of a Valorant player |
+| v!weapons  | Display weapon stats of a Valorant player |
+| v!map     | Display map stats of a Valorant player |
+| v!link | Link a Valorant user to your Discord ID    |
+| v!unlink | Unlink a Valorant user from your Discord ID    |
+| v!linked | Check which Valorant account has been linked to your Discord ID    |
+| v!invite | Get Discord bot invite link |
+
+| Extra        | Description    | 
+| ------------- |-------------| 
+| v!joke      | Tell a joke      | 
+| v!ping      | Pong!             | 
+| v!help      | Display commands   |
+
+First Discord bot project. Created by Brandon Vo.  
+Retrieves data from https://tracker.gg/valorant  
+Built with Discord.js, uses MongoDB database to store usernames
+
---
+++ commands/help.js
@@ -0,0 +1,114 @@
+const { MessageEmbed } = require("discord.js");
+const pagination = require("discord.js-pagination");
+
+module.exports = {
+  name: "help",
+  description: "Display avaliable commands and other helpful information to the user.",
+
+  execute(message) {
+    const helpEmbed1 = new MessageEmbed()
+      .setColor("RANDOM")
+      .setAuthor("Help Menu", "https://bit.ly/32GAtTp")
+      .addFields(
+        {
+          name: "How to use this Discord bot",
+          value:
+            "```fix\n" +
+            "1. Sign in with your Riot ID on https://tracker.gg/valorant\n" +
+            "2. Track your Valorant stats with the following commands\n```" +
+            "```yaml\n" +
+            "         Test Server: https://discord.gg/8bY6nFaVEY" +
+            "\n```",
+        },
+        {
+          name: "\u200B",
+          value:
+            "```md\n" +
+            "#                      Bot Commands                        #" +
+            "\n```",
+        },
+        {
+          name: "<:normal:840652327160381460> Competitive Stats `v!stats username#tag` | `v!comp username#tag`",
+          value: "Display competitive career stats of a user",
+        },
+        {
+          name: "<:normal:840652327160381460> Unrated Stats `v!unrated username#tag`",
+          value: "Display unrated career stats of a user",
+        },
+        {
+          name: "<:spikerush:840652327447035934> Spike Rush Stats `v!sr username#tag` | `v!spikerush username#tag`",
+          value: "Display unrated career stats of a user",
+        },
+        {
+          name: "<:deathmatch:840652327231946803> Deathmatch Stats `v!dm username#tag` | `v!deathmatch username#tag`",
+          value: "Display deathmatch career stats of a user",
+        },
+        {
+          name: "<:escalation:840652327139540993> Escalation Stats `v!escalation username#tag`",
+          value: "Display escalation career stats of a user",
+        },
+        {
+          name: ":leftwards_arrow_with_hook: Last Game `v!lm username#tag`",
+          value: "Display last match information of a user",
+        },
+        {
+          name: "<:jett:839142770576851006> Agents Played `v!agent username#tag`",
+          value: "Display competitive agent stats of a user",
+        },
+        {
+          name: "<:vandal:861082251508580394> Weapon Stats `v!weapon username#tag`",
+          value: "Display competitive weapon stats of a user",
+        },
+        {
+          name: "<:ascent:854916943194619934> Map Stats `v!map username#tag`",
+          value: "Display competitive map stats of a user"
+        },
+        {
+          name: ":link: Link Account `v!link username#tag` | `v!unlink` | `v!linked`",
+          value: "Link/Unlink a Valorant account to/from your Discord ID",
+        },
+        {
+          name: ":e_mail: Invite Link `v!invite`",
+          value: "Get Discord bot invite link",
+        },
+        { name: ":white_circle: Extra `v!joke` | `v!ping` | `v!help`", value: "\u200B" }
+      )
+      .setTimestamp();
+
+    const helpEmbed2 = new MessageEmbed()
+      .setColor("RANDOM")
+      .setAuthor("Help Menu", "https://bit.ly/32GAtTp")
+      .addFields(
+        {
+          name: "\u200B",
+          value:
+            "```md\n" +
+            "#                         Other                           #" +
+            "\n```",
+        },
+        {
+          name: "Notes",
+          value:
+            "- The bot must have access to the default permissions in order to function properly\n" + 
+            "- You can view stats of a linked user by mentioning their username in the command",
+        },
+        { name: "KDR", value: "kills ÷ deaths", inline: true },
+        { name: "KDA", value: "(kills + (assists ÷ 2)) ÷ deaths", inline: true },
+        { name: "KAD", value: "(kills + assists) ÷ deaths", inline: true },
+        { name: "Known Bugs", value: "Stats not being retrieved - Try Again", inline: true },
+        { name: "Found Bugs? ", value: "Contact CMDRVo#3496 on Discord", inline: true  },
+        { name: "Common Mistake", value: "When linking an account to your Discord ID, you must type the command properly.\n" +
+        "Do not include `< >` in between your username and tag. Not case sensitive.\n" + 
+        "Command: `v!link username#tag`" + "\n" + "Examples: `v!link sen tenz#0505` `v!link CMDRVo#CMDR` `v!link 100t aSUnA#1111`"},
+      )
+      .setTimestamp();
+
+    const helpPages = [helpEmbed1, helpEmbed2]; // Pages
+
+    const flipPage = ["⬅️", "➡️"]; // Flip Pags
+
+    const timeout = "100000"; // Timeout
+
+    pagination(message, helpPages, flipPage, timeout);
+  },
+};
